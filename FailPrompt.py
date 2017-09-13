da0abd5632ca197427e93d758b23334916d461e6
Evan Dixon <evanldixon@live.com>
2017-09-13 10:12:50 -0500

    Change refusals from random to sequential & fix whitespace Spaces are now used instead of tabs

--- FailPrompt.py
+++ FailPrompt.py
@@ -2,46 +2,50 @@ import random
 
 # Commands
 def instructions():
-	print("Type a command, then press enter.")
+    print("Type a command, then press enter.")
 
 def ping():
-	print("Pong!")
+    print("Pong!")
 
 def matrix():
     for i in range(0, 1000000):
         print(random.randrange(0, 9), end = '')
 
-def quit():
-	keepGoing = False
-
-commands = { "instructions": instructions, 
-			 "ping": ping,
-             "matrix": matrix,
-			 "quit": quit }
-
 # Variables
+commands = { "instructions": instructions, 
+             "ping": ping,
+             "matrix": matrix
+             }
+
 responses = ("Eh, I don't feel like it.",
-			 "I don't wanna",
-			 "Maybe later",
-			 "Why should I?",
-			 "Get someone else to do it"
-			 )
+             "I don't wanna",
+             "Maybe later",
+             "Why should I?",
+             "Get someone else to do it")
 
-keepGoing = True
+def main():    
+    commandName = "instructions"
+    nextResponse = 0
+    while True:
+        # Do the command
+        if commandName in commands:
+            command = commands[commandName]
+            if random.randrange(1, 100) > 60:
+                print(responses[nextResponse])
+                nextResponse += 1
+                if nextResponse >= len(responses):
+                    nextResponse = 0
+            else:
+                command()
+        elif commandName == "quit":
+            break
+        else:
+            print("Unknown command '" + commandName + "'")
 
-def main():	
-	commandName = "instructions"
-	while keepGoing:
-		# Do the command
-		command = commands[commandName]
-		if random.randrange(1, 100) > 60:
-			print(responses[random.randrange(0, len(responses) - 1)])
-		else:
-			command()
-
-		# Prompt for the next command
-		print("> ", end="")
-		commandName = input()
+        # Prompt for the next command
+        print("> ", end="")
+        commandName = input()
+    # end while
 # end main
 
 
